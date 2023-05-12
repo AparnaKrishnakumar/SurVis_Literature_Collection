74ca0ed15592e6d17b9b169bd2440f914f4e6224
AparnaKrishnakumar <56480522+AparnaKrishnakumar@users.noreply.github.com>
2023-05-12 11:31:38 +0100

    Add files via upload

---
+++ src/data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿define({ entries : {
+    "Chen2017Agent-based": {
+        "author": "Chen, Y.",
+        "doi": "https://doi.org/10.1007/s10586-017-1134-7",
+        "journal": "Cluster Computing",
+        "keywords": "type:model, agent-based_research, crowd_interaction, emergency_evacuation",
+        "number": "1",
+        "pages": "189-202",
+        "title": "Agent-based research on crowd interaction in emergency evacuation",
+        "type": "article",
+        "volume": "23",
+        "year": "2017"
+    },
+    "Edrisi2021Simulating": {
+        "author": "Edrisi, A. and Lahoorpoor, B. and Lovreglio, R.",
+        "doi": "10.1016/j.cstp.2021.06.011",
+        "journal": "Case Studies on Transport Policy",
+        "keywords": "type:research, emergency_evacuation, metro_station, agent-based_exit_choice_models, simulation",
+        "number": "3",
+        "title": "Simulating Metro Station Evacuation Using Three Agent-based Exit Choice Models",
+        "type": "article",
+        "volume": "9",
+        "year": "2021"
+    },
+    "Gwynne1999Review": {
+        "author": "Gwynne, S. and Galea, E.R. and Owen, M. and Lawrence, P.J. and Filippidis, L.",
+        "doi": "https://doi.org/10.1016/s0360-1323(98)00057-2",
+        "journal": "Building and Environment",
+        "keywords": "type:review, evacuation_simulation, built_environment",
+        "number": "6",
+        "pages": "741-749",
+        "title": "A review of the methodologies used in the computer simulation of evacuation from the built environment",
+        "type": "article",
+        "volume": "34"
+    },
+    "Li2018Numerical": {
+        "author": "Li, S. and Yu, X. and Zhang, Y. and Zhai, C.",
+        "doi": "https://doi.org/10.1016/j.physa.2017.08.058",
+        "journal": "Physica A: Statistical Mechanics and its Applications",
+        "keywords": "type:simulation, occupant_evacuation, earthquake, casualty_prediction",
+        "pages": "1238-1250",
+        "title": "A numerical simulation strategy on occupant evacuation behaviors and casualty prediction in a building during earthquakes",
+        "type": "article",
+        "volume": "490",
+        "year": "2018"
+    },
+    "Li2022Metro": {
+        "author": "Li, H. and Wang, Y. and Jiang, J. and Zhou, R.",
+        "doi": "10.1177/00375497221095071",
+        "journal": "SIMULATION",
+        "keywords": "type:research, emergency_evacuation, metro_station, safety_assessment, emergency_response",
+        "number": "10",
+        "publisher": "SAGE Publications",
+        "title": "Metro Station Evacuation Safety Assessment Considering Emergency Response",
+        "type": "article",
+        "volume": "98",
+        "year": "2022"
+    },
+    "Mandal2022Simulation": {
+        "archiveprefix": "arXiv",
+        "author": "Mandal, T. and Rao, K.R. and Tiwari, G.",
+        "eprint": "2209.06187",
+        "keywords": "type:research, emergency_evacuation, metro_station, passengers_with_disabilities, simulation",
+        "primaryclass": "physics",
+        "title": "Simulation of Emergency Evacuation of Passengers with and without Disability at Different Types of Metro Stations",
+        "type": "article",
+        "year": "2022"
+    },
+    "Sun2022Heterogeneous": {
+        "author": "Sun, R. and Wu, W.",
+        "doi": "https://doi.org/10.1109/ITSC55140.2022.9922479",
+        "journal": "IEEE Xplore",
+        "keywords": "type:model, heterogeneous_cellular_automata, crowd_evacuation, emergency_situations",
+        "title": "A Heterogeneous Cellular Automata Model for Crowd Evacuation in Emergency Situations",
+        "type": "article",
+        "year": "2022"
+    },
+    "Yu2018Passengers": {
+        "author": "Yu, L. and Deng, T. and Wang, M. and Li, Q. and Xu, S.",
+        "doi": "https://doi.org/10.1080/23248378.2018.1552210",
+        "journal": "International Journal of Rail Transportation",
+        "keywords": "type:simulation, evacuation, railway_tunnel, fire",
+        "number": "3",
+        "pages": "159-172",
+        "title": "Passengers\u2019 evacuation from a fire train in railway tunnel",
+        "type": "article",
+        "volume": "7",
+        "year": "2018"
+    },
+    "Zheng2019Simulation": {
+        "author": "Zheng, Y. and Li, X.-G. and Jia, B. and Jiang, R.",
+        "doi": "https://doi.org/10.1016/j.simpat.2019.03.001",
+        "journal": "Simulation Modelling Practice and Theory",
+        "keywords": "type:simulation, pedestrians, evacuation, flood_spreading, cellular_automaton",
+        "pages": "149-161",
+        "title": "Simulation of pedestrians\u2019 evacuation dynamics with underground flood spreading based on cellular automaton",
+        "type": "article",
+        "volume": "94",
+        "year": "2019"
+    },
+    "Zou2019Agent-based": {
+        "author": "Zou, Q. and Fernandes, D.S. and Chen, S.",
+        "doi": "10.1080/19439962.2019.1634661",
+        "journal": "Journal of Transportation Safety & Security",
+        "keywords": "type:research, emergency_evacuation, subway_train, platform, agent-based_simulation",
+        "number": "3",
+        "title": "Agent-based Evacuation Simulation from Subway Train and Platform",
+        "type": "article",
+        "volume": "13",
+        "year": "2019"
+    }
+}});
